a5818afd7458071151c97c2fde4eeddf93967be4
whoisdavidd <whoisdavidd@hotmail.com>
2024-01-27 00:00:47 +0800

    TicketBooking kind of done

---
+++ my-app/src/main/java/com/mycompany/app/Ticket.java
@@ -0,0 +1,12 @@
+public class Ticket {
+    private String eventSelection;
+    private int numberoftickets;
+    private double Paymentdetails;
+
+    public Ticket(String eventSelection, int numberoftickets, double Paymentdetails){
+        this.eventSelection = eventSelection;
+        this.numberoftickets = numberoftickets;
+        this.Paymentdetails = Paymentdetails;
+    }
+    
+}
