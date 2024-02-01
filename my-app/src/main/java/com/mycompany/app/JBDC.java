63a901e4deccc5bed5a665a0e66b47eff211aa0e
whoisdavidd <whoisdavidd@hotmail.com>
2024-02-01 19:40:08 +0800

    working sql

--- my-app/src/main/java/com/mycompany/app/JBDC.java
+++ my-app/src/main/java/com/mycompany/app/JBDC.java
@@ -9,7 +9,6 @@ public class JBDC {
         //insert password here
         String password = "";
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection connection = DriverManager.getConnection(url,username,password);
 
@@ -24,6 +23,84 @@ public class JBDC {
         }
         catch (Exception e){
             System.out.println(e);
-        }
+        }  
     }
+    //  // Create a new record
+    //  static void createRecord(String name, int age) {
+    //     try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    //         String insertQuery = "INSERT INTO student (name, age) VALUES (?, ?)";
+    //         try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+    //             preparedStatement.setString(1, name);
+    //             preparedStatement.setInt(2, age);
+    //             int affectedRows = preparedStatement.executeUpdate();
+    //             System.out.println("Record created, affected rows: " + affectedRows);
+    //         }
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // // Read all records
+    // static void readRecords() {
+    //     try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    //         Statement statement = connection.createStatement();
+    //         ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
+
+    //         while (resultSet.next()) {
+    //             System.out.println(resultSet.getInt("id") + " " +
+    //                     resultSet.getString("name") + " " +
+    //                     resultSet.getInt("age"));
+    //         }
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // // Update a record
+    // static void updateRecord(int id, String name, int age) {
+    //     try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    //         String updateQuery = "UPDATE student SET name=?, age=? WHERE id=?";
+    //         try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+    //             preparedStatement.setString(1, name);
+    //             preparedStatement.setInt(2, age);
+    //             preparedStatement.setInt(3, id);
+    //             int affectedRows = preparedStatement.executeUpdate();
+    //             System.out.println("Record updated, affected rows: " + affectedRows);
+    //         }
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // // Delete a record
+    // static void deleteRecord(int id) {
+    //     try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    //         String deleteQuery = "DELETE FROM student WHERE id=?";
+    //         try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+    //             preparedStatement.setInt(1, id);
+    //             int affectedRows = preparedStatement.executeUpdate();
+    //             System.out.println("Record deleted, affected rows: " + affectedRows);
+    //         }
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+//     public class JDBCExample {
+//         // Database connection parameters
+//         static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+//         static final String USERNAME = "root";
+//         static final String PASSWORD = "";
+    
+//         public static void main(String[] args) {
+//             // Example CRUD operations
+//             createRecord("John Doe", 25);
+//             readRecords();
+//             updateRecord(1, "Updated Name", 30);
+//             readRecords();
+//             deleteRecord(1);
+//             readRecords();
+//         }
+// }
 }
+
+
