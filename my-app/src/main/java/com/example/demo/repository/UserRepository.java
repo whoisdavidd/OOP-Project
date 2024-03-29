9974399430cbbe4afdd149a08e8bce578c7edff6
David <whoisdavidd@hotmail.com>
2024-03-29 13:31:18 +0800

    sorry import the optional

--- my-app/src/main/java/com/example/demo/repository/UserRepository.java
+++ my-app/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
