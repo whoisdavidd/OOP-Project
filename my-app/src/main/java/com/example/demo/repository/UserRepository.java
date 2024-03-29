2704cafbb7756a420bd021c6a95ae5aecfe57d47
David <whoisdavidd@hotmail.com>
2024-03-29 13:30:04 +0800

    update user repository

--- my-app/src/main/java/com/example/demo/repository/UserRepository.java
+++ my-app/src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,4 +9,5 @@ import com.example.demo.entityFile.Users.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
+    Optional<User> findByUsername(String username);
 }
