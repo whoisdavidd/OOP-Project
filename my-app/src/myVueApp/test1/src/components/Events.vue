4cdea2a1983261a4b7af4b5d9b84ea58e63cc3e9
gnzat <100507662+gnzat@users.noreply.github.com>
2024-04-08 01:12:46 +0800

    WIP on taz/ui: 60ababb Merge branch 'taz/ui' of https://github.com/whoisdavidd/OOP-Project into taz/ui (#5)

--- my-app/src/myVueApp/test1/src/components/Events.vue
+++ my-app/src/myVueApp/test1/src/components/Events.vue
@@ -1,40 +1,105 @@
 <template>
-    <div style="margin-top:56px">
-      <div class="tabs">
+  <div style="margin-top: 56px">
+    <h2 class="text-center p-3">Events</h2>
+    <div class="dropdown">
+      <button
+        class="btn dropdown-toggle"
+        type="button"
+        id="filterEvents"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Filter
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="filterEvents">
+        <li><h6 class="dropdown-header">Filter events</h6></li>
+        <li>
+          <button
+            @click="currentTab = 'all'"
+            class="dropdown-item form-control"
+            default
+          >
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            @click="currentTab = 'concerts'"
+            class="dropdown-item form-control"
+            default
+          >
+            Concerts
+          </button>
+        </li>
+        <li>
+          <button
+            @click="currentTab = 'movies'"
+            class="dropdown-item form-control"
+            default
+          >
+            Movies
+          </button>
+        </li>
+        <li>
+          <button
+            @click="currentTab = 'sportsEvents'"
+            class="dropdown-item form-control"
+            default
+          >
+            Sports Event
+          </button>
+        </li>
+      </ul>
+    </div>
+    <!-- <div class="tabs">
         <button @click="currentTab = 'all'">All</button>
         <button @click="currentTab = 'concerts'">Concerts</button>
         <button @click="currentTab = 'movies'">Movies</button>
         <button @click="currentTab = 'sportsEvents'">Sports Events</button>
-      </div>
-      <div v-if="currentTab === 'all'">
-        <h2>All Events</h2>
-        <ul>
-          <li v-for="event in events" :key="event.id">{{ event.name }}</li>
-        </ul>
-      </div>
-      <div v-else-if="currentTab === 'concerts'">
-        <h2>Concerts</h2>
-        <ul>
-          <li v-for="concert in concerts" :key="concert.id">{{ concert.name }}</li>
-        </ul>
-      </div>
-      <div v-else-if="currentTab === 'movies'">
-        <h2>Movies</h2>
-        <ul>
-          <li v-for="movie in movies" :key="movie.id">{{ movie.name }}</li>
-        </ul>
-      </div>
-      <div v-else-if="currentTab === 'sportsEvents'">
-        <h2>Sports Events</h2>
-        <ul>
-          <li v-for="sportsEvent in sportsEvents" :key="sportsEvent.id">{{ sportsEvent.name }}</li>
-        </ul>
+      </div> -->
+    <div>
+      <div class="row">
+        <div
+          class="col-md-4 justify-content-md-between"
+          v-for="event in filteredEvents"
+          :key="event.id"
+        >
+          <div class="card mx-3 my-3" style="height: 30rem">
+            <div></div>
+            <img
+              :src="require(`../img/events/${event.eventName}.jpg`)"
+              class="card-img-top img-fluid overflow-hidden"
+              alt="..."
+              style="object-fit: contain; height: 300rem; width: 200rem"
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center">{{ event.eventName }}</h5>
+              <p class="card-text text-center">
+                Event details:
+                <br /><br />
+                Date: {{ event.eventDate }}<br />
+                Time: {{ event.eventTime }} <br />
+                <span v-if="event.duration"
+                  >Duration: {{ event.duration }}<br
+                /></span>
+                Venue: {{ event.eventVenue }}<br />
+                <span v-if="event.rating">Rating: {{ event.rating }}</span>
+              </p>
+              <!-- link to a book now page -->
+              <button class="btn btn-dark" @click="bookTicket(event)">
+                Book now!
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+
 
 export default {
   name: "AllEvents",
@@ -45,7 +110,8 @@ export default {
       concerts: [],
       movies: [],
       sportsEvents: [],
-    }
+      bookEvent: "",
+    };
   },
   mounted() {
     this.getEvents();
@@ -61,34 +127,65 @@ export default {
         this.events = this.events.concat(response.data);
         
       } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error("Error fetching events:", error);
       }
     },
-   async getAllConcerts() {
-    try {
-        const response = await axios.get('http://localhost:8080/api/concert');
-        this.concerts = response.data
-    } catch (error) {
-        console.error('Error fetching concerts', error);
-    }
-   },
-   async getAllMovies() {
-    try {
-        const response = await axios.get('http://localhost:8080/api/movie');
-        this.movies = response.data
-    } catch (error) {
-        console.error('Error fetching movies', error);
-    }
-   },
-   async getAllSportsEvent() {
-    try {
-        const response = await axios.get('http://localhost:8080/api/SportsEvent');
-        this.sportsEvents = response.data
-    } catch (error) {
-        console.error('Error fetching sports events', error);
-    }
-   },
-  }
+    async getAllConcerts() {
+      try {
+        const response = await axios.get("http://localhost:8080/api/concert");
+        this.concerts = response.data;
+      } catch (error) {
+        console.error("Error fetching concerts", error);
+      }
+    },
+    async getAllMovies() {
+      try {
+        const response = await axios.get("http://localhost:8080/api/movie");
+        this.movies = response.data;
+      } catch (error) {
+        console.error("Error fetching movies", error);
+      }
+    },
+    async getAllSportsEvent() {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/SportsEvent"
+        );
+        this.sportsEvents = response.data;
+      } catch (error) {
+        console.error("Error fetching sports events", error);
+      }
+    },
+    bookTicket(bookEvent) {
+      const username = sessionStorage.getItem("user");
+      if (!username || username == null) {
+        alert("Please log into your account first");
+        this.$router.push("/login");
+      } else {
+        try {
+          this.$router.push({path: "/booking-page", params: {
+            eventId: bookEvent.eventId
+          } });
+        } catch (err) {
+          console.log("An error occurred: " + err);
+        }
+      }
+    },
+  },
+  computed: {
+    filteredEvents() {
+      if (this.currentTab == "all") {
+        return this.events;
+      } else if (this.currentTab == "concerts") {
+        return this.concerts;
+      } else if (this.currentTab == "movies") {
+        return this.movies;
+      } else if (this.currentTab == "sportsEvents") {
+        return this.sportsEvents;
+      }
+      return this.events;
+    },
+  },
 };
 </script>
 
